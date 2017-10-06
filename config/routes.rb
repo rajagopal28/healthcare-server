4b85a618d3bb70ea8d87e83a48dba2e6877a11df
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-06 17:34:30 +0530

    Adding home controller for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :medicine_intake_logs
   resources :doctor_notifications
   resources :user_vital_logs
@@ -10,4 +12,6 @@ Rails.application.routes.draw do
   resources :doctors
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to => 'home#index'
 end
