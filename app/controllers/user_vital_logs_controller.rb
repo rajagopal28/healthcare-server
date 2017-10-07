0da60e1ef62213f34a89bd50694ec930593e5966
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-07 13:47:17 +0530

    Adding user lookup in user vital logs views

--- app/controllers/user_vital_logs_controller.rb
+++ app/controllers/user_vital_logs_controller.rb
@@ -1,11 +1,16 @@
 class UserVitalLogsController < ApplicationController
   before_action :set_user_vital_log, only: [:show, :edit, :update, :destroy]
   before_action :set_users, only: [:new, :edit]
+  before_action :set_user_from_session, only: [:index, :new]
 
   # GET /user_vital_logs
   # GET /user_vital_logs.json
   def index
-    @user_vital_logs = UserVitalLog.all
+    if @user
+      @user_vital_logs = UserVitalLog.where(user_id: @user.id)
+    else
+      @user_vital_logs = UserVitalLog.all
+    end
   end
 
   # GET /user_vital_logs/1
@@ -70,6 +75,21 @@ class UserVitalLogsController < ApplicationController
 
     def set_users
       @users = User.all
+    end
+
+    def set_user_from_session
+      #initialise ids
+      user_id = nil
+
+      # find user
+      if session[:user_id]
+        user_id = session[:user_id]
+      elsif request.query_parameters['userId']
+        user_id = request.query_parameters['userId']
+      end
+      if user_id
+        @user = User.find(user_id)
+      end
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_vital_log_params
