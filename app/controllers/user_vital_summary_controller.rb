41c3c5fe75e2a4e5c38ff70c0d16dc09570946bf
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-09 18:29:14 +0530

    Adding status check logic based on values in user vitals summary

--- app/controllers/user_vital_summary_controller.rb
+++ app/controllers/user_vital_summary_controller.rb
@@ -7,11 +7,30 @@ class UserVitalSummaryController < ApplicationController
       else
         logs = UserVitalLog.group(:user_id).select([:user_id, "AVG(pressure) as pressure", "AVG(sugar) as sugar", "AVG(temperature) as temperature", "AVG(pulse) as pulse"])
       end
-      @user_vital_summary = logs.map{|uv| UserVitalSummary.new(user_vital_log: uv, pressure_status: 'Normal', sugar_status: 'Normal', temperature_status: 'Normal', pulse_status: 'Normal') }
+      @user_vital_summary = logs.map{|uv| UserVitalSummary.new(user_vital_log: uv, pressure_status: get_pressure_status(uv.pressure), sugar_status: get_sugar_status(uv.sugar), pulse_status: get_pulse_status(uv.pulse), temperature_status: get_temperature_status(uv.temperature)) }
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def get_pressure_status(value)
+      return value < 50 ? 'Low' : ( value < 100? 'Normal' : 'High')
+    end
+
+
+    def get_sugar_status(value)
+      return value < 50 ? 'Low' : ( value < 100? 'Normal' : 'High')
+    end
+
+
+    def get_temperature_status(value)
+      return value < 50 ? 'Low' : ( value < 100? 'Normal' : 'High')
+    end
+
+
+    def get_pulse_status(value)
+      return value < 50 ? 'Low' : ( value < 100? 'Normal' : 'High')
+    end
+
     def set_user_from_session
       #initialise ids
       user_id = nil
