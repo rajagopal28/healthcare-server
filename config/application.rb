456a902369ec795d0342845b47215b5dfa84eb76
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2017-10-11 10:55:35 +0530

    Changing timezone to chennai to resolve issues from client

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module HealthcareServer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Chennai'
+    config.active_record.default_timezone = :local # Or :utc
   end
 end
